6c13d8fd878b3836cbd8fd632cc703942e1b3561
shatanov <timur-shatanov.work@mail.ru>
2021-04-18 20:04:24 +0300

    add js logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,98 @@
-const btnTask = document.querySelectorAll('.btn__task');
+const inputBtn = document.querySelector('.input__btn');
 const textTask = document.querySelectorAll('.body__text');
+const input = document.querySelector('.header__input');
+const tasksWrapper = document.querySelector('.tasks__wrapper');
+const tasksInfo = document.querySelector('.tasks__info');
+
+let tasksAll = []
 
 
 
+const addCompletedTask = (btn, bodyText) => {
+    if(btn.classList.contains('task__btn--compleate') && bodyText.classList.contains('body__text--compleate')) {
+        btn.classList.remove('task__btn--compleate')
+        bodyText.classList.remove('body__text--compleate')
 
-
-const completedTask = (i) => {
-    if(btnTask[i].classList.contains('task__btn--compleate')){
-        btnTask[i].classList.remove('task__btn--compleate')
     } else {
-        btnTask[i].classList.add('task__btn--compleate')
+        btn.classList.add('task__btn--compleate')
+        bodyText.classList.add('body__text--compleate')
     }
-    // if(btnTask[i] != 1)
+}
+
+const clearInput = () => {
+    input.value = '';
+    if(inputBtn.classList.contains('task__btn--compleate')){
+        inputBtn.classList.remove('task__btn--compleate')
+    }
+}
+
+const addTask = () => {
+    const task = {
+        compleated: checkCompleated(),
+        text: input.value
+    }
+    tasksAll.push(task)
+    clearInput();
+    addElemDOM()
+}
+
+const checkCompleated = () =>{
+    if(inputBtn.classList.contains('task__btn--compleate')){
+        return true
+    } else {
+       return false
+    }
+}
+
+const addElemDOM =  () => {
+    const taskWrapper = document.createElement('div');
+    taskWrapper.classList.add('task__wrapper')
+    const btnWrapper = document.createElement('div');
+    btnWrapper.classList.add('btn__wrapper');
+    const btn = document.createElement('button');
+    btn.classList.add('btn__task');
+
+
+    const taskBody = document.createElement('div');
+    taskBody.classList.add('task__body');
+    const bodyText = document.createElement('div');
+    bodyText.classList.add('body__text');
+    const delWrapper = document.createElement('div');
+    delWrapper.classList.add('body__del');
+    delBtn = document.createElement('button');
+    delBtn.classList.add('del__btn');
+
+
+    btnWrapper.appendChild(btn);
+    taskWrapper.appendChild(btnWrapper);
+    delWrapper.appendChild(delBtn);
+    taskBody.appendChild(bodyText);
+    taskBody.appendChild(delWrapper);
+    taskWrapper.appendChild(taskBody);
+
+    for(let i=0; i < tasksAll.length; i++) {
+        if(tasksAll[i].compleated == true) {
+            btn.classList.add('task__btn--compleate')
+            bodyText.classList.add('body__text--compleate')
+        }
+        bodyText.textContent = tasksAll[i].text;
+        tasksWrapper.insertBefore(taskWrapper, tasksInfo);
+        btn.addEventListener('click', () => addCompletedTask(btn, bodyText))
+    }
 }
 
 
+input.addEventListener('keydown', (e) => {
+    if(e.keyCode == 13){
+        addTask()
+    }
+});
 
-for(let i = 0; i < btnTask.length; i++){
-    btnTask[i].addEventListener('click', () => completedTask(i))
-}
+inputBtn.addEventListener('click', () => {
+    if(inputBtn.classList.contains('task__btn--compleate')) {
+        inputBtn.classList.remove('task__btn--compleate')
+    } else {
+        inputBtn.classList.add('task__btn--compleate')
+    }
+})
+
