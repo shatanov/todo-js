6099b1305e42564c45f8697f989c924b5754f279
shatanov <timur-shatanov.work@mail.ru>
2021-04-20 19:35:18 +0300

    add cheking

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ const tasksWrapper = document.querySelector('.tasks__wrapper');
 const taskWrapperFirst = document.querySelector('.task__wrapper');
 const tasksInfo = document.querySelector('.tasks__info');
 const tasksCount = document.querySelector('.count');
+const allBtn = document.querySelector('.all__btn');
+const activeBtn = document.querySelector('.active__btn');
+const completedBtn = document.querySelector('.completed__btn');
+const infoBtn = document.querySelectorAll('.info__link');
 
 let tasksAll = [
     // {
@@ -15,9 +19,21 @@ let tasksAll = [
     //     compleated: false,
     //     text: '33'
     // }
-]
-let newTaskCount = 0
-tasksCount.textContent = tasksAll.length + " "
+];
+
+let rewritesTask = []
+
+let newTaskCount = 0;
+tasksCount.textContent = tasksAll.length + " ";
+
+
+const checkInput = (text) => {
+    if(text === ''){
+        return false
+    } else {
+        return true
+    }
+}
 
 const addCompletedTask = (btn, bodyText) => {
     if(btn.classList.contains('task__btn--compleate') && bodyText.classList.contains('body__text--compleate')) {
@@ -33,7 +49,7 @@ const addCompletedTask = (btn, bodyText) => {
             t.compleated = !t.compleated
         }
     })
-}
+};
 
 const delTask = (taskWrapper, bodyText) => {    
     taskWrapper.remove()
@@ -43,30 +59,69 @@ const delTask = (taskWrapper, bodyText) => {
             tasksAll.splice(idx, 1);
         }
     })
-    updateTasksCount()
-}
+    updateTasksCount();
+    newTaskCount -= 1;
+};
+
+// const resetBtn = () => {
+//     if(allBtn.classList.contains('info__link--active') || 
+//     activeBtn.classList.contains('info__link--active') ||
+//     completedBtn.classList.contains('info__link--active')){
+//         infoBtn.forEach(b => {
+//             b.classList.remove('info__link--active')
+//         });
+//         addNewTask()
+//     }
+// }
+
 
 const updateTasksCount = () => {
     tasksCount.textContent = tasksAll.length + " "
-}
+};
 
 const clearInput = () => {
     input.value = '';
     if(inputBtn.classList.contains('task__btn--compleate')){
         inputBtn.classList.remove('task__btn--compleate')
     }
-}
+};
 
 const addTask = () => {
     const task = {
         compleated: checkCompleated(),
         text: input.value
     }
-    tasksAll.push(task);
-    updateTasksCount();
-    clearInput();
-    addElemDOM()
-}
+    if(checkInput(input.value)){
+        tasksAll.push(task);
+        updateTasksCount();
+        clearInput();
+        addElemDOM();
+    } else {
+        alert('ERROR')
+    }  
+};
+
+// const checkBtn = (btn) => {
+//     if(btn.classList.contains('info__link--active')){
+//         btn.classList.remove('info__link--active');
+//         addNewTask();
+//     } else {
+//        btn.classList.add('info__link--active');
+//         if(btn.classList.contains('all__btn')){
+//             rewritesTask = tasksAll.slice()
+//         }
+//         if(btn.classList.contains('active__btn')){
+//             rewritesTask = tasksAll.filter(t => t.compleated == false);
+//         }
+//         if(btn.classList.contains('completed__btn')){
+//             rewritesTask = tasksAll.filter(t => t.compleated == true);
+//         }
+//     }
+// }
+
+// const updateElement = (task) => {
+
+// };
 
 const checkCompleated = () =>{
     if(inputBtn.classList.contains('task__btn--compleate')){
@@ -74,25 +129,25 @@ const checkCompleated = () =>{
     } else {
        return false
     }
-}
+};
 
 const addElemDOM = () => {
     if(tasksAll.length > 1){
         newTaskCount += 1
         addNewTask()
     } else {
+        taskWrapperFirst.remove();
         addNewTask()
     }
-}
+};
 
 const addNewTask = () => {
     for(let i = newTaskCount; i < tasksAll.length; i++){
         createElement(i)
     }
-}
+};
 
 const createElement = (i) => {
-    taskWrapperFirst.remove();
     const taskWrapper = document.createElement('div');
     taskWrapper.classList.add('task__wrapper')
     const btnWrapper = document.createElement('div');
@@ -126,7 +181,7 @@ const createElement = (i) => {
     tasksWrapper.insertBefore(taskWrapper, tasksInfo);
     btn.addEventListener('click', () => addCompletedTask(btn, bodyText))
     delBtn.addEventListener('click', () => delTask(taskWrapper, bodyText))
-}
+};
 
 
 input.addEventListener('keydown', (e) => {
@@ -141,6 +196,9 @@ inputBtn.addEventListener('click', () => {
     } else {
         inputBtn.classList.add('task__btn--compleate')
     }
-})
+});
 
+// allBtn.addEventListener('click', () => checkBtn(allBtn));
+// activeBtn.addEventListener('click', () => checkBtn(activeBtn));
+// completedBtn.addEventListener('click', () => checkBtn(completedBtn));
 
